ec6e2f23636c5b483ed5680b5206fa1504af8634
Johannes la Poutré <tech@tabworld.co>
2016-03-08 14:51:23 +0100

    Initial checkin

---
+++ BootstrapflatAsset.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * 2016-03-08
+ * @author Squio <info@squio.nl>
+ * https://bootflat.github.io/
+ */
+namespace squio\theme\bootstrapflat;
+
+use yii\web\AssetBundle;
+class BootstrapflatAsset extends AssetBundle
+{
+    public $sourcePath='@vendor/squio/yii2-theme-bootstrapflat/assets';
+    public $baseUrl = '@web';
+    
+    public $css=[
+        'css/bootstrap-flat.min.css',
+        'css/bootstrap-flat-extras.min.css',
+    ];
+    
+//     public $js=[
+//         'js/icheck.min.js',
+//     ];
+//     
+//     public $img=[
+//     	'img/check_flat/default.png',
+//     ]; 
+    
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+    
+    public function init() {
+        parent::init();
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+Yii2  Flat Theme 
+======================
+Theme for Yii2 Web Applicaiton for yii2.0
+
+Fully based on [bootflat](https://scottdorman.github.io/bootstrap-flat/) - v3.3.4
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist squio/yii2-theme-bootstrapflat "*"
+```
+
+or add
+
+```
+"squio/yii2-theme-bootstrapflat": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+```
+use squio\theme\bootstrapflat;
+
+bootstrapflat\BootstrapflatAsset::register($this);
+```
